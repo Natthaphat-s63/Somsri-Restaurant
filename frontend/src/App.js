fc02846215ae06b90f902df0cdfa28ee4b7846d1
NatthaphatKMUTNB <87507926+NatthaphatKMUTNB@users.noreply.github.com>
2021-10-03 00:21:50 +0700

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Form from './components/Form';
+import ShowQueue from './components/ShowQueue'
+
+function App() {
+  return (
+    <div>
+      <ShowQueue/>
+      <div>
+        <Form/>
+      </div>
+    </div>
+
+        
+        
+  );
+}
+
+export default App;
