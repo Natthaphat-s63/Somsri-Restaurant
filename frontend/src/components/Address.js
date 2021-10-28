efe48270e16fb07e60de7739b3305c00e0850d9d
NatthaphatKMUTNB <87507926+NatthaphatKMUTNB@users.noreply.github.com>
2021-10-28 21:16:20 +0700

    Add files via upload

---
+++ frontend/src/components/Address.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Address.css'
+const addr=()=>{
+    return(
+        <div className = "container">
+            <div className ='addr'>
+
+                <h1>Opening hour</h1>
+                <br/>
+                10.00 - 22.00
+                <br/>
+                <br/>
+                <h2>Address</h2>
+                123 Xxxxxxx,
+                        Xxxxxxxxx,<br/>
+                        Bangkok,
+                        12345
+                <h2>Tel</h2>
+                02xxxxxxxx
+                </div>
+        </div>
+    );
+}
+export default addr;
